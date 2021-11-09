374c82dada581e941b60859a1d49429ce6a5c06e
seenkoo <shinkupro@gmail.com>
2021-11-09 14:18:40 +0500

    switch from sqlite3 to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_08_105157) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exchange_rates", force: :cascade do |t|
     t.string "from_type"
     t.string "to_type"
